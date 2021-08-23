c76e89be2d6be4b29f6ec9e9f69e3678f3f965e5
ARBaiysh <o0052i@mail.ru>
2021-08-23 09:20:43 +0600

    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.luv2code.ecommerce.dao;
 
-import com.luv2code.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin({"http://192.168.24.1:4200","http://localhost:4200", "http://192.168.24.103:4200"})
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
